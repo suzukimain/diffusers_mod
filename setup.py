49a03d713375e3181db0ae4784ea4d4eef2a88eb
suzukimain <131413573+suzukimain@users.noreply.github.com>
2023-11-02 12:40:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 !pip install git+https://github.com/huggingface/diffusers.git
-import shutil
-shutil.move("./safety_checker.py", "/usr/local/lib/python3.10/dist-packages/diffusers/pipelines/deepfloyd_if/safety_checker.py")
+print('''safety_checker.pyを次のパスにコピーしてください
+      "/usr/local/lib/python3.10/dist-packages/diffusers/pipelines/deepfloyd_if/safety_checker.py"
+      ''')
