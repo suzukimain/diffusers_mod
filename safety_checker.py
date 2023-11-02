5f84582e1f866db2cc94944595a2ca9dbfba070c
suzukimain <131413573+suzukimain@users.noreply.github.com>
2023-11-02 16:29:50 +0900

    Update safety_checker.py

--- safety_checker.py
+++ safety_checker.py
@@ -10,40 +10,22 @@ from ...utils import logging
 logger = logging.get_logger(__name__)
 
 
-safety_level = 0.5  # 初期値を0.5に設定
-
-def safety_checker_level(level):
-    global safety_level  # グローバル変数としてsafety_levelを使用することを宣言
-    safety_level = level
-    if not 0.1 <= safety_level <= 0.9:
-        safety_level = 0.5
-        logger.warning(
-        "'safety_checker_level()'には、0.1~0.9の引数が必要です"
-        )
-    return safety_level
-
-
-
 class IFSafetyChecker(PreTrainedModel):
     config_class = CLIPConfig
-
     _no_split_modules = ["CLIPEncoderLayer"]
-
+    safety_level = 0.5
     def __init__(self, config: CLIPConfig):
         super().__init__(config)
-
         self.vision_model = CLIPVisionModelWithProjection(config.vision_config)
-
         self.p_head = nn.Linear(config.vision_config.projection_dim, 1)
         self.w_head = nn.Linear(config.vision_config.projection_dim, 1)
 
     @torch.no_grad()
-    def forward(self, clip_input, images, p_threshold=safety_level, w_threshold=safety_level):
+    def forward(self, clip_input, images):
         image_embeds = self.vision_model(clip_input)[0]
-
         nsfw_detected = self.p_head(image_embeds)
         nsfw_detected = nsfw_detected.flatten()
-        nsfw_detected = nsfw_detected > p_threshold
+        nsfw_detected = nsfw_detected > IFSafetyChecker.safety_level # クラス変数を参照
         nsfw_detected = nsfw_detected.tolist()
 
         if any(nsfw_detected):
@@ -58,7 +40,7 @@ class IFSafetyChecker(PreTrainedModel):
 
         watermark_detected = self.w_head(image_embeds)
         watermark_detected = watermark_detected.flatten()
-        watermark_detected = watermark_detected > w_threshold
+        watermark_detected = watermark_detected > IFSafetyChecker.safety_level # クラス変数を参照
         watermark_detected = watermark_detected.tolist()
 
         if any(watermark_detected):
